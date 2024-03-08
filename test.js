0fb051ddaafbf77003cbe326625f1025d3e2460f
Miguel Salinas <miguel.salinase10@gmail.com>
2024-03-09 00:33:44 +0900

    add example of object in JS

--- test.js
+++ test.js
@@ -2,3 +2,14 @@ let message
 message = "hi";
 
 console.log(message);
+
+let user = {
+    name: "John",
+    age: 30, 
+    "likes birds": true  // multiword property name must be quoted
+};
+
+delete user["likes birds"]; // remove property
+delete user.age; // remove property
+
+user.age = 25; // add property
