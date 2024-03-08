08aee2d8c42ccaaab9d8275ef3ca656ef1302ca1
Miguel Salinas <miguel.salinase10@gmail.com>
2024-03-09 04:17:59 +0900

    add property value shorthand example

--- test.js
+++ test.js
@@ -13,3 +13,21 @@ delete user["likes birds"]; // remove property
 delete user.age; // remove property
 
 user.age = 25; // add property
+
+// This is how we would normally create an object 
+function makeUser(name, age) {
+    return {
+        name: name,
+        age: age
+    };
+} 
+
+// Using property value shorthand, we can do this instead
+function makeUser(name, age) {
+    return {
+        name, // same as name: name
+        age   // same as age: age
+    };
+}
+
+
